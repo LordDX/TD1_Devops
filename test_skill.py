2a88a32a1746b2868bfbd02eef0c2c1330e0399d
Karadian <tvignot009@gmail.com>
2022-03-15 11:00:12 +0100

    Commit Devops TD 1 - tests unitaires skill

---
+++ test_skill.py
@@ -0,0 +1,16 @@
+import unittest
+import Skill
+from unittest.mock import Mock
+
+
+class TestSkills(unittest.TestCase):
+
+    def testLvl(self):
+        skill = Skill("test")
+        with self.assertRaises(Exception):
+            skill.setLvl("o")
+
+    def testType(self):
+        skill = Skill("test")
+        with self.assertRaises(Exception):
+            skill.setType("o")
